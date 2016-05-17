a3e75690f47cfd6b84fc0f3599411b55c38d74c7
Alex H. Wagner <awagner@genome.wustl.edu>
2016-05-17 15:59:48 -0500

    complete initial support for substitutions from protein to c and g coordinates

--- hgvs.py
+++ hgvs.py
@@ -1,5 +1,6 @@
 import re
 import requests
+import string
 # from Bio.Data import CodonTable
 # from Bio.SeqUtils import seq1, seq3
 
@@ -8,19 +9,19 @@ __author__ = 'Alex H Wagner'
 
 class HgvsBase:
 
-    def __init__(self, ref_seq_id, start, stop, ref, alt, mut_type, predicted=False):
+    def __init__(self, ref_seq_id, start, stop, ref, alt, edit_type, predicted=False):
         self.ref_seq_id = ref_seq_id
         self.start = str(start)
         self.stop = str(stop)
         self.ref = str(ref)
         self.alt = str(alt)
-        self.mut_type = mut_type
+        self.edit_type = edit_type
         self.predicted = predicted
         if not self._validate():
             raise ValueError
 
     def _validate(self):
-        if self.mut_type == 'substitution':
+        if self.edit_type == 'substitution':
             return all((self.ref_seq_id.isalnum(),
                         self.ref.isalpha(),
                         self.start.isnumeric(),
@@ -36,12 +37,24 @@ class HgvsBase:
     def hgvs(self):
         raise NotImplementedError
 
+    @property
+    def info(self):
+        d = {'id': self.ref_seq_id,
+             'start': self.start,
+             'stop': self.stop,
+             'ref': self.ref,
+             'alt': self.alt,
+             'edit_type': self.edit_type,
+             'predicted': self.predicted,
+             'hgvs': self.hgvs}
+        return d
+
 
 class P(HgvsBase):
 
     @property
     def hgvs(self):
-        if self.mut_type == 'substitution':
+        if self.edit_type == 'substitution':
             return "{}:p.{}{}{}".format(self.ref_seq_id, self.ref, self.start, self.alt)
 
 class C(HgvsBase):
@@ -50,7 +63,7 @@ class C(HgvsBase):
 
     @property
     def hgvs(self):
-        if self.mut_type == 'substitution':
+        if self.edit_type == 'substitution':
             return "{}:{}.{}{}>{}".format(self.ref_seq_id, self.prefix, self.start, self.ref, self.alt)
 
 
@@ -58,7 +71,7 @@ class G(C):
 
     prefix = 'g'
 
-    def __init__(self, ref_seq_id, chromosome, start, stop, strand, ref, alt, mut_type):
+    def __init__(self, ref_seq_id, chromosome, start, stop, strand, ref, alt, edit_type):
         self.ref_seq_id = ref_seq_id
         self.start = str(start)
         self.stop = str(stop)
@@ -66,17 +79,54 @@ class G(C):
         self.alt = str(alt)
         self.chromosome = chromosome
         self.strand = str(strand)
-        self.mut_type = mut_type
+        self.edit_type = edit_type
         self._validate()
+
+    @property
+    def info(self):
+        d = {'id': self.ref_seq_id,
+             'start': self.start,
+             'stop': self.stop,
+             'ref': self.ref,
+             'alt': self.alt,
+             'chromosome': self.chromosome,
+             'strand': self.strand,
+             'edit_type': self.edit_type,
+             'hgvs': self.hgvs}
+        return d
+
+    @property
+    def ucsc(self):
+        if self.chromosome.isnumeric():
+            chromosome = 'chr' + self.chromosome
+        else:
+            chromosome = self.chromosome
+        return str('{}:{}-{}'.format(chromosome, self.start, self.stop))
 
 
 class Variant:
 
-    regex = re.compile(r'(?P<id>\S+):(?P<prefix>[pgc])\.(?P<edits>.*)')
-    p_sub_regex = re.compile(r'(?P<res1>[a-zA-Z]{1,3})(?P<pos>\d+)(?P<res2>[a-zA-Z*=]{1,3})')
-    p_ins_regex = re.compile(r'abracadabra')  # TODO: Change this to something sensible.
+    _primary_regex = re.compile(r'(?P<id>\S+):(?P<prefix>[pgc])\.(?P<edits>.*)')
+    _p_sub_regex = re.compile(r'(?P<res1>[a-zA-Z]{1,3})(?P<pos>\d+)(?P<res2>[a-zA-Z*=]{1,3})')
+    _p_ins_regex = re.compile(r'abracadabra')  # TODO: Change this to something sensible.
+    _session = requests.Session()
+
+    def __repr__(self):
+        return self.hgvs
+
+    @property
+    def hgvs(self):
+        return self.g.hgvs, self.c.hgvs, self.p.hgvs
+
+    @property
+    def info(self):
+        return self.g.info, self.c.info, self.p.info
+
+    def __str__(self):
+        return str(self.__repr__())
 
     def __init__(self, hgvs=None, reference_assembly='current', species='human'):
+        self.local = False  # TODO: Add support for local annotation files
         self.species = species
         self.p = None
         self.c = None
@@ -93,7 +143,7 @@ class Variant:
             self._subdomain = reference_assembly.lower() + '.'
         else:
             raise ValueError("reference_build should be of the format XX or grchXX (e.g. 'grch37')")
-        m = Variant.regex.match(hgvs)
+        m = Variant._primary_regex.match(hgvs)
         if not m:
             raise ValueError("hgvs string ({}) not recognized.".format(hgvs))
         self.edit = m.group('edits')
@@ -101,16 +151,16 @@ class Variant:
             raise ValueError("hgvs alleles are currently not supported.")
         prefix = m.group('prefix')
         if prefix == 'p':
-            if Variant.p_sub_regex.match(self.edit):
+            if Variant._p_sub_regex.match(self.edit):
                 self.edit_type = 'substitution'
-                p_edit_match = Variant.p_sub_regex.match(self.edit)
+                p_edit_match = Variant._p_sub_regex.match(self.edit)
                 start = p_edit_match.group('pos')
                 stop = start
                 ref = p_edit_match.group('res1')
                 alt = p_edit_match.group('res2')
-            elif Variant.p_ins_regex.match(self.edit):
+            elif Variant._p_ins_regex.match(self.edit):
                 self.edit_type = 'insertion'
-                p_edit_match = Variant.p_ins_regex.match(self.edit)
+                p_edit_match = Variant._p_ins_regex.match(self.edit)
                 # TODO: Finish filling out start, stop, ref, alt
             else:
                 raise ValueError("This type of protein change ({}) is not currently supported.")
@@ -127,34 +177,69 @@ class Variant:
     def _vep_hgvs_rest(self, hgvs):
         url = 'http://{}rest.ensembl.org/vep/{}/hgvs/{}?content-type=application/json'\
             .format(self._subdomain, self.species, hgvs)
-        resp = requests.get(url)
+        resp = self._session.get(url)
         resp.raise_for_status()
         return resp.json()[0]
 
     def _p_fill(self):
         try:
-            r = self._vep_hgvs_rest(self.p.hgvs)
-        except requests.HTTPError as e:
-            msg = e.response.content.decode()
-            raise ValueError('VEP REST lookup failed: {}'.format(msg))
-        candidate_transcripts = r['transcript_consequences']
-        try:
-            transcript = self._select_best_vep_transcript(candidate_transcripts)
+            t = self._select_best_vep_transcript(self.p.hgvs)
 
-            self.c = C(transcript['transcript_id'],
-                       transcript['cds_start'],
-                       transcript['cds_end'],
-                       r['allele_string'].split('/')[0],
-                       r['allele_string'].split('/')[1],
+            self.c = C(t['transcript_id'],
+                       t['cds_start'],
+                       t['cds_end'],
+                       t['codons'].split('/')[0].translate(str.maketrans('', '', string.ascii_lowercase)),
+                       t['codons'].split('/')[1].translate(str.maketrans('', '', string.ascii_lowercase)),
                        self.edit_type)
-
-            self.g = G(transcript['transcript_id'],
-                       r['start'],
-                       transcript['cds_end'])
         except ValueError as e:
             raise e  # TODO: Add in call to Ensembl overlap if no transcripts are returned by VEP query
 
-    def _select_best_vep_transcript(self, transcripts):
+        self._c_to_g()
+        self.p.ref_seq_id = self._get_transcript_info(self.c.ref_seq_id)['Translation']['id']
+
+    def _c_to_g(self):
+        t = self._map_cds_to_genome()
+
+        self.g = G(self.c.ref_seq_id,
+                   t['seq_region_name'],
+                   t['start'],
+                   t['end'],
+                   t['strand'],
+                   self.c.ref,
+                   self.c.alt,
+                   self.edit_type)
+
+    def _c_to_p(self):
+        t_info = self._get_transcript_info(self.c.ref_seq_id)
+        p_id = t_info['Translation']['id']
+        self.p = None  #TODO: Go from Ensembl ID and coordinates to a protein change. Use BioPython and http://goo.gl/40sMGm
+
+
+    def _get_transcript_info(self, transcript_id):
+        """Return a dictionary conforming to the responses here: http://rest.ensembl.org/documentation/info/lookup"""
+        if not self.local:
+            try:
+                url = 'http://{}rest.ensembl.org/lookup/id/{}?content-type=application/json;expand=1'\
+                        .format(self._subdomain, transcript_id)
+                resp = self._session.get(url)
+                resp.raise_for_status()
+            except requests.HTTPError as e:
+                msg = e.response.content.decode()
+                raise ValueError('Ensembl ID Lookup query failed: {}'.format(msg))
+            return resp.json()
+        else:
+            raise ValueError('Local annotations not yet supported.')
+
+    def _select_best_vep_transcript(self, hgvs):
+        if not self.local:
+            try:
+                r = self._vep_hgvs_rest(hgvs)
+            except requests.HTTPError as e:
+                msg = e.response.content.decode()
+                raise ValueError('VEP REST query failed: {}'.format(msg))
+            transcripts = r['transcript_consequences']
+        else:
+            raise ValueError('Local annotations not yet supported.')
         result = list()
         backup = list()
         for i, transcript in enumerate(transcripts):
@@ -182,7 +267,25 @@ class Variant:
             raise ValueError("No matching transcripts in VEP response!")
         return transcripts[best]
 
+    def _map_cds_to_genome(self):
+        if not self.local:
+            try:
+                url = 'http://{}rest.ensembl.org/map/cds/{}/{}..{}?content-type=application/json'\
+                    .format(self._subdomain, self.c.ref_seq_id, self.c.start, self.c.stop)
+                resp = self._session.get(url)
+                resp.raise_for_status()
+            except requests.HTTPError as e:
+                msg = e.response.content.decode()
+                raise ValueError('Ensembl CDS Map query failed: {}'.format(msg))
+            result = resp.json()['mappings']
+            if len(result) != 1:
+                raise ValueError('Expected exactly one mapping for transcript {}, received {}.'
+                                 .format(self.c.ref_seq_id, len(result)))
+        else:
+            raise ValueError('Local annotations not yet supported.')
+        return result[0]
+
 
 if __name__ == '__main__':
     v = Variant('FGFR3:p.R248C', reference_assembly=37)
-    print(v.c)
+    print(v)
--- test_variant.py
+++ test_variant.py
@@ -5,11 +5,24 @@ __author__ = 'Alex H Wagner'
 
 
 class TestVariant(TestCase):
-    def setUp(self):
-        self.p_sub = hgvs.Variant('FGFR3:p.R248C')
+    @classmethod
+    def setUpClass(cls):
+        cls.p_sub = hgvs.Variant('FGFR3:p.R248C', reference_assembly=37)
+        cls.p_sub_neg = hgvs.Variant('ALK:p.F1174I', reference_assembly=37)
 
     def test_p_sub_to_c(self):
         assert self.p_sub.c.hgvs == 'ENST00000352904:c.742C>T'
 
+    def test_p_sub_rep(self):
+        assert self.p_sub.hgvs == ('ENST00000352904:g.1803564C>T',
+                                   'ENST00000352904:c.742C>T',
+                                   'ENSP00000231803:p.R248C')
+
     def test_p_sub_to_g(self):
-        assert self.p_sub.g.hgvs
+        assert self.p_sub.g.hgvs == 'ENST00000352904:g.1803564C>T'
+
+    def test_ucsc(self):
+        assert self.p_sub.g.ucsc == 'chr4:1803564-1803564'
+
+    def test_negative_strand_rep(self):
+        assert self.p_sub_neg.hgvs == ()
